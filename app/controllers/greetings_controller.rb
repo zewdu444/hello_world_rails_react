536c07ba93302bc26b809a9025b732a07a36cdef
zewdu erkyhun <zewdu444@gmail.com>
2023-06-05 15:20:09 +0300

    Add api and views for greetings

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -3,7 +3,7 @@ class GreetingsController < ApplicationController
   def index
     @greeting = Greeting.order("RANDOM()").first
     respond_to do |format|
-      format html
+      format.html
       format.json { render json: @greeting }
       format.xml { render xml: @greeting }
     end
